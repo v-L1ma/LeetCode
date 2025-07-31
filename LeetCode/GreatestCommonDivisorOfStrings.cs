d79d45b7624c41a5fcbc4d041c92c1a002c6e938
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2025-07-30 21:46:43 -0300

    Initial commit

---
+++ LeetCode/GreatestCommonDivisorOfStrings.cs
@@ -0,0 +1,7 @@
+namespace LeetCode;
+// LeetCode 1071. Greatest Common Divisor of Strings
+
+public class GreatestCommonDivisorOfStrings
+{
+    
+}
---
+++ LeetCode/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] nums = new []{1,1,1,2,2,3};
+int k = 2;
+
+Dictionary<int, int> top = new Dictionary<int, int>();
+
+for(int i=0; i<nums.Length;i++)
+{
+    if (top.ContainsKey(nums[i]))
+    {
+        top[nums[i]] += 1;
+    }
+    else
+    {
+        top.Add(nums[i], 1);
+    }
+}
+
+var freq = 
+
+nums = freq.ToArray();
